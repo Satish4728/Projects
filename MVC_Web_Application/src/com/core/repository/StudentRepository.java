d6873edc1d26cb831a166fa80186ae07a2fcf4f1
Satish Reddy <165980217+Satish4728@users.noreply.github.com>
2024-07-04 21:52:19 +0530

    Delete MVC_Web_Application directory

--- MVC_Web_Application/src/com/core/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.core.repository;
-
-public interface StudentRepository {
-    public String add();
-    public String search();
-    public String update();
-    public String delete();
-}
